b1c9a504ecda13d087322fc7d4d6780068ec0e02
srbaron <baron.steven2012@gmail.com>
2020-11-10 16:06:04 -0700

    added card button, reset react template

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import CardButton from './components/UI/CardButton';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Seven Wonders
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CardButton btnText='Host Game'></CardButton>
+        <CardButton btnText='Join Game'></CardButton>
       </header>
     </div>
   );
---
+++ src/components/UI/CardButton.js
@@ -0,0 +1,14 @@
+import css from './CardButton.css';
+
+function CardButton(props) {
+    return (
+      <div className='card-button-container'>
+          <button className='card-button'>
+            {props.btnText}
+          </button>
+      </div>
+    );
+  }
+  
+  export default CardButton;
+  
